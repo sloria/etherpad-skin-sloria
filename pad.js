16c1dfde5453274259411d1c9a8731b58d4a02b3
Steven Loria <sloria1@gmail.com>
2025-10-13 23:50:22 -0400

    Initial commit

---
+++ pad.js
@@ -0,0 +1,9 @@
+function customStart() {
+  $("#pad_title").show();
+  $(".buttonicon").mousedown(function () {
+    $(this).parent().addClass("pressed");
+  });
+  $(".buttonicon").mouseup(function () {
+    $(this).parent().removeClass("pressed");
+  });
+}
